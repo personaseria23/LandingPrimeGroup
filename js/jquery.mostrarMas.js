7e49a757adadfbf921b526a6ac41b8a28339b2ec
personaseria23 <charliespotifai@gmail.com>
2022-07-18 13:19:42 -0400

    fix 18/07

--- js/jquery.mostrarMas.js
+++ js/jquery.mostrarMas.js
@@ -2,11 +2,11 @@
 $('#abrirMAM').on('click', function () {
     console.log("estás aquí");  
     $('.nav-tabs a[href="#tab-6"]').tab('show');
-    document.getElementById("features").scrollIntoView();
+    document.getElementById("tab-6").scrollIntoView();
   })
   
 $('#abrirECom').on('click', function () {
     console.log("estás aquí");  
     $('.nav-tabs a[href="#tab-2"]').tab('show');
-    document.getElementById("features").scrollIntoView();
+    document.getElementById("tab-2").scrollIntoView();
   })
