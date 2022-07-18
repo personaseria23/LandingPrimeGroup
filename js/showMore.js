2ef9028c996b51ae461f04652e9aafdcba326752
personaseria23 <charliespotifai@gmail.com>
2022-07-18 01:23:34 -0400

    fix 18/07

--- js/showMore.js
+++ js/showMore.js
@@ -1,15 +1,20 @@
 let readMore_btn = document.getElementById('readMore_btn');
 let hideText = document.getElementById('hideText');
+let youtubeMAM= document.getElementById('youtubeMAM');
 
 readMore_btn.addEventListener('click', toggleText);
 
 function toggleText() {
   hideText.classList.toggle('showText');
-
+  console.log("estás aquí");
+  youtubeMAM.style.display="inline";
+  console.log("u hir");
   if(hideText.classList.contains('showText')) {
+
     readMore_btn.innerHTML = 'Leer menos'
   }
   else {
     readMore_btn.innerHTML = 'Saber más'
+    youtubeMAM.style.display="none";
   }
 }
