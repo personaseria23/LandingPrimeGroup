6cfe0a0f64cddafd9a678317f244e499eba618e2
personaseria23 <charliespotifai@gmail.com>
2022-07-19 13:05:45 -0400

    fix 19/07

--- js/showMore.js
+++ js/showMore.js
@@ -4,20 +4,39 @@ let youtubeMAM= document.getElementById('youtubeMAM');
 let ecomerce= document.getElementById('leerMasECom');//botón
 let imgHide= document.getElementById('imgHide');
 let botonMAM= document.getElementById('abrirMAM');//imagen
-
+let xq = document.getElementById('xqECom');
 readMore_btn.addEventListener('click', toggleText);
 ecomerce.addEventListener('click', mostrarImagen);
 
 
 
 function mostrarImagen(){
-if(imgHide.style.display === 'none'){
-  imgHide.style.display = 'inline';
-  ecomerce.innerHTML='Leer menos';
-}else{
-  imgHide.style.display='none';
-  ecomerce.innerHTML='Saber Más';
-}
+  imgHide.classList.toggle('showText');//texto
+  console.log("estás aquí");
+  xq.style.display="inline";
+  console.log("u hir");
+  if(imgHide.classList.contains('showText')) {
+
+    ecomerce.innerHTML = 'Leer menos'
+  }
+  else {
+    ecomerce.innerHTML = 'Saber más'
+    xq.style.display="none";
+  }
+
+
+
+
+
+
+  //if(imgHide.style.display === 'none'){
+//  imgHide.style.display = 'inline';
+//  xq.style.display='inline';
+//  ecomerce.innerHTML='Leer menos';
+//}else{
+//  imgHide.style.display='none';
+//  ecomerce.innerHTML='Saber Más';
+//}
 }
 function toggleText() {
   hideText.classList.toggle('showText');//texto
