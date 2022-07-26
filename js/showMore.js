d85aa550192e0fb397fdd5bc1a5e2ae6b99a3717
personaseria23 <charliespotifai@gmail.com>
2022-07-26 12:46:50 -0400

    fix 26/07

--- js/showMore.js
+++ js/showMore.js
@@ -1,81 +1,78 @@
-let readMore_btn = document.getElementById('readMore_btn');//botón
 let hideText = document.getElementById('hideText');//texto
-let youtubeMAM= document.getElementById('youtubeMAM');
-let ecomerce= document.getElementById('leerMasECom');//botón
+
 let imgHide= document.getElementById('imgHide');
 let botonMAM= document.getElementById('abrirMAM');//imagen
-let xq = document.getElementById('xqECom');
+
 let sap = document.getElementById('sapHide');
 let sapMas= document.getElementById('sapMore');
+
+
+//divs
+let divMam = document.getElementById('divMam');
+let divSrm = document.getElementById('divSrm');
+let divEcom = document.getElementById('divEcom');
+let divSap = document.getElementById('divSap');
+//botones
+let botonSrm = document.getElementById('saberSrm');
+let readMore_btn = document.getElementById('readMore_btn');
+let botonEcom= document.getElementById('leerMasECom');
+let botonSap = document.getElementById('botonSap');
+//imagenes
+let imgEcom = document.getElementById('porQue');
+let youtubeMAM= document.getElementById('youtubeMAM');
 let sapImg= document.getElementById('imgSAP');
-let mamTxt= document.getElementById('ulMam');
-let mamTxt1= document.getElementById('ulMam1');
-let botonMenos= document.getElementById('readMore_btn2');
-
-sapMas.addEventListener('click',mostrarImgSAP);
-readMore_btn.addEventListener('click', toggleText);
-ecomerce.addEventListener('click', mostrarImagen);
-botonMenos.addEventListener('click', mostrarMenosMam);
-
-  
 
 
-function mostrarImgSAP(){
-  
-  sap.classList.toggle('showText');//texto
-  console.log("estás aquí");
-  sapImg.style.display="inline";
-  console.log("u hir");
-  if(sap.classList.contains('showText')) {
 
-    sapMas.innerHTML = 'Leer menos'
-  }
-  else {
-    sapMas.innerHTML = 'Saber más'
-    sapImg.style.display="none";
+
+
+
+
+
+
+function handleClickMam() {
+  console.log('estás aquí1');
+  if (divMam.style.display === "none" || divMam.style.display === "") {
+    divMam.style.display = 'inline';
+    youtubeMAM.style.display = 'inline';
+
+    readMore_btn.textContent = 'Leer Menos';
+  } else {
+    divMam.style.display = 'none';
+    youtubeMAM.style.display = 'none';
+    readMore_btn.textContent = 'Saber Más';
   }
 }
+function handleClickSrm() {
+  if (divSrm.style.display === "none" || divSrm.style.display === "") {
+    divSrm.style.display = 'inline';
 
+    botonSrm.textContent = 'Leer Menos';
+  } else {
+    divSrm.style.display = 'none';
 
-
-
-
-
-
-function mostrarImagen(){
-  imgHide.classList.toggle('showText');//texto
-  console.log("estás aquí");
-  xq.style.display="inline";
-  console.log("u hir");
-  if(imgHide.classList.contains('showText')) {
-
-    ecomerce.innerHTML = 'Leer menos'
-  }
-  else {
-    ecomerce.innerHTML = 'Saber más'
-    xq.style.display="none";
+    botonSrm.textContent = 'Saber Más';
   }
 }
-
-function toggleText() {
-  mamTxt.style.display="inline";
-  mamTxt1.style.display="inline";//texto
-  console.log("estás aquí");
-  youtubeMAM.style.display="inline";
-  console.log("u hir");
-  if(mamTxt.style.display=='inline') {
-    
-    readMore_btn.style.display = 'none'
-    botonMenos.style.display= 'inline';
-  }
-  else {
-    readMore_btn.innerHTML = 'Saber más//'
-    youtubeMAM.style.display="none";
+function handleClickEcom() {
+  if (divEcom.style.display === "none" || divEcom.style.display === "") {
+    divEcom.style.display = 'inline';
+    imgEcom.style.display = 'inline';
+    botonEcom.textContent = 'Leer Menos';
+  } else {
+    divEcom.style.display = 'none';
+    imgEcom.style.display = 'none';
+    botonEcom.textContent = 'Saber Más';
   }
 }
-function mostrarMenosMam() {
-  mamTxt.style.display="none";
-  mamTxt1.style.display="none";
-  youtubeMAM.style.display="none";
-  botonMenos.innerHTML = 'Saber mas'
+function handleClickSap() {
+  if (divSap.style.display === "none" || divSap.style.display === "") {
+    divSap.style.display = 'inline';
+    sapImg.style.display = 'inline';
+    botonSap.textContent = 'Leer Menos';
+  } else {
+    divSap.style.display = 'none';
+    sapImg.style.display = 'none';
+    botonSap.textContent = 'Saber Más';
+  }
 }
